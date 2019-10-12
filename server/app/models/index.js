8c42f27e3016ebb266baa826d26b024efb9730c4
Efen Stakes <efenstakes101@gmail.com>
2019-10-12 14:42:50 +0300

    add a server models bootstrap file

---
+++ server/app/models/index.js
@@ -0,0 +1,7 @@
+const Agency =  require('./agency')
+const Staff =  require('./gov-staff')
+const FosterRequest =  require('./foster-request')
+
+module.exports = {
+    Agency, Staff, FosterRequest
+}
