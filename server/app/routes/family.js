3869e5bd5ecced0be6ba22ae762b9578fcc8a7c0
Efen Stakes <efenstakes101@gmail.com>
2019-10-13 02:50:56 +0300

    add routes and logic for family login

---
+++ server/app/routes/family.js
@@ -0,0 +1,23 @@
+// include external libraries
+const Router = require('express').Router()
+const passport = require('passport')
+
+
+// include external libraries
+
+
+
+Router.post('login',  async (req, res)=> {
+    let response =  { token: null }
+
+    // create the user token data
+    let token_data = { id: req.user.ref, timestamp: Date.now() }
+    let token = Jwt.sign({ data: token_data }, process.env.JWT_SECRET)
+
+    // set the token and user data to be returned in response
+    response.token = token
+
+
+    res.json(response)
+})
+
